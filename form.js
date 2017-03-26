7b71f56f99856c8743003be5afbb5bccdb1271a4
Niсk_Elixir <free_x@vfemail.net>
2017-03-26 15:16:53 +0300

    Updating and adding new modules

--- form.js
+++ form.js
@@ -2,20 +2,11 @@ class Form {
     constructor(name, elements, legend, submitFunc, submitElem) {
         this.name = name;
         this.elements = elements;
-        if (legend) {
-            this.legend = legend;
-        }
-        if (submitFunc) {
-            this.submitFunc = submitFunc;
-        } else {
-            alert("There is no submit function! Please add to this.submitFunc.");
-        }
-        if (submitElem) {
-            this.submitElem = submitElem;
-        } else {
-            alert("Therу is no submit element! It will be made automatically.");
-            this.submitElem = new Submit(this.name + "-submit", "Отправить");
-        }
+        this.legend = legend ? legend : null;
+        this.submitFunc = submitFunc ? submitFunc : null;
+            //If there is no submit function, please add to this.submitFunc.
+            this.submitElem = submitElem ? submitElem : new Submit(this.name + "-submit", "SUBMIT");
+            //If there is no submit element, it will be made automatically.
         this.render();
     }
     render() {
@@ -27,14 +18,21 @@ class Form {
             form.appendChild(legend);
         }
         for (let i in this.elements) {
-            if (this.elements[i] instanceof Note) {
-                form.appendChild(this.elements[i].elem);
-            } else {
-                let div = document.createElement("div");
-                div.className = "textfield";
-                let label = document.createElement("label");
-                label.innerHTML = this.elements[i].header;
-                div.appendChild(label);
+            if (this.elements[i] instanceof Note) form.appendChild(this.elements[i].elem);
+            else {
+                if (this.elements[i] instanceof Checkbox) {
+                    var div = document.createElement("p");
+                    div.className = "checkbox";
+                    div.innerHTML = this.elements[i].header;
+                    div.addEventListener("click", function(){
+                        if (event.target.tagName == "P") event.target.children[0].checked = !event.target.children[0].checked;
+                    });
+                } else {
+                    var div = document.createElement("div");
+                    let label = document.createElement("label");
+                    label.innerHTML = this.elements[i].header;
+                    div.appendChild(label);
+                }
                 div.appendChild(this.elements[i].elem);
                 form.appendChild(div);
             }
@@ -42,34 +40,23 @@ class Form {
         form.addEventListener("submit", this.submitFunc);
         this.submitElem.render();
         form.appendChild(this.submitElem.elem);
-
         this.elem = form;
     }
 }
-class Input {
-    constructor(name, header, options, parent) { //parent - родитель или нет (есть наследник-класс или нет).
+class Input extends Element{
+    constructor(name, header, options, parent) {
+        super("input", options);
         this.name = name;
-        if (!this.type) {
-            this.type = "text";
-        }
+        this.type = this.type ? this.type : "text";
         this.header = header;
-        if (options) {
-            this.options = options;
-        }
         if (!parent) {
             this.render();
         }
     }
     render() {
-        let input = document.createElement("input");
-        input.name = this.name;
-        input.type = this.type;
-        if (this.options) {
-            for (let i in this.options) {
-                input.setAttribute(i, this.options[i]);
-            }
-        }
-        this.elem = input;
+        super.render();
+        this.elem.name = this.name;
+        this.elem.type = this.type;
     }
 }
 class Num extends Input {
@@ -111,24 +98,28 @@ class Radio extends Input {
         }
         p.appendChild(input);
         p.appendChild(document.createTextNode(this.header));
-        p.addEventListener("click", focusRadioParagraph);
+        p.addEventListener("click", function(event) {
+            if (event.target.tagName == "P") {
+                event.target.children[0].checked = true;
+            }
+        });
         this.elem = p;
     }
 }
-class RadioList {
-    constructor(header, elements) {
-        this.elements = elements;
+class RadioList extends Element {
+    constructor(header, items) {
+        super("div");
+        this.items = items;
         this.header = header;
         this.render();
     }
     render() {
-        let div = document.createElement("div");
-        for (let i in this.elements) {
-            let radio = new Radio(this.elements[i].name, this.elements[i].header, {value : this.elements[i].value});
+        super.render();
+        for (let i in this.items) {
+            let radio = new Radio(this.items[i].name, this.items[i].header, {value : this.items[i].value});
             radio.render();
-            div.appendChild(radio.elem);
+            this.elem.appendChild(radio.elem);
         }
-        this.elem = div;
     }
 }
 class Submit extends Input {
@@ -142,21 +133,19 @@ class Submit extends Input {
         this.elem.value = this.header;
     }
 }
-class Select {
-    constructor(name, Options, header) {
+class Select extends Element{
+    constructor(name, Options, header, options) {
+        super("select", options);
         this.name = name;
         this.Options = Options;
         this.header = header;
         this.render();
     }
     render() {
-        let select = document.createElement("select");
-        select.name = this.name;
+        super.render();
+        this.elem.name = this.name;
         this.renderOptions();
-        for (let i in this.renderedOptions) {
-            select.appendChild(this.renderedOptions[i]);
-        }
-        this.elem = select;
+        for (let i in this.renderedOptions) this.elem.appendChild(this.renderedOptions[i]);
     }
     renderOptions() {
         let options = [];
